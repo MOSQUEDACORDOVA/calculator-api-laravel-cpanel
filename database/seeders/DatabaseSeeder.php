c4a906e6b0be992a12184412a9b1c4571c344b2c
Isaac Mosqueda <elmosquedacordova@gmail.com>
2026-01-30 18:21:14 -0500

    initials

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,31 +3,21 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        $this->call([
-            CurrencySeeder::class,
-        ]);
-
         // User::factory(10)->create();
 
-            User::updateOrCreate(
-                ['email' => 'tester@example.com'],
-                [
-                    'name' => 'API Tester',
-                    'password' => Hash::make('password123'),
-                ]
-            );
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
     }
 }
